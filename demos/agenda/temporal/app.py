97a021ceab5c85834108a406df2eea27850b14df
ERICK MONTIEL GARCIA <1725110382@utectulancingo.edu.mx>
2026-07-06 13:42:58 +0000

    UPDATED temporal

--- demos/agenda/temporal/app.py
+++ demos/agenda/temporal/app.py
@@ -4,8 +4,10 @@ urls = ("/.*", "hello")
 app = web.application(urls, globals())
 
 class hello:
-    def GET(self):
-        return 'Hello, Dejah'
+    def GET(self, name):
+        if not name:
+            bane = "World"
+        return 'Hello, ' + name + '!'
 
 if __name__ == "__main__":
     app.run()
