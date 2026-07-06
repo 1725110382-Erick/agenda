6625b47a3511eb50770c48991d59c56086f01b6f
ERICK MONTIEL GARCIA <1725110382@utectulancingo.edu.mx>
2026-07-06 13:44:09 +0000

    UPDATED agenda

--- demos/agenda/controllers/ver_contacto.py
+++ demos/agenda/controllers/ver_contacto.py
@@ -6,5 +6,6 @@ render = web.template.render('views', base='layout')
 class VerContacto:
 
     def GET(self, id_contacto):
+        print(f"ID_contacto: {id_contacto}")
 
         return render.ver_contacto()
